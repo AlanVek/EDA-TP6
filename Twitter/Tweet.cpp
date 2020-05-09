036de1629c3115c36328b13936e5f15d1bf49597
Alan Vekselman <alanvek@hotmail.com>
2020-05-09 00:17:27 -0300

    Updated date string.

--- Twitter/Tweet.cpp
+++ Twitter/Tweet.cpp
@@ -1,6 +1,20 @@
 #include "Tweet.h"
 
-Tweet::Tweet(std::string username_, std::string content_, std::string date_) : username(username_), content(content_), date(date_) {};
+const char* extraString = "+0000";
+const int extraStrSize = strlen(extraString);
+
+Tweet::Tweet(std::string username_, std::string content_, std::string date_) : username(username_), content(content_)
+{
+	//Finds '+0000' structure that comes in API date form.
+	int pos = date_.find(extraString);
+
+	//If there was a '+0000', it removes it.
+	if (pos != std::string::npos)
+		date = date_.substr(0, pos) + date_.substr(pos + extraStrSize + 1, date_.length() - pos - 1 - extraStrSize);
+
+	else
+		date = date_;
+};
 
 Tweet::~Tweet() {};
 
